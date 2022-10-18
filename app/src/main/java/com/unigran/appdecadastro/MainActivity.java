2c15859f83e2f0b18d7bbcd8c3ef8da30c55cc79
Nathan Fusinato <nathanfusinato@outlook.com>
2022-10-18 19:40:41 -0400

    Adicionado codigo das activity

--- app/src/main/java/com/unigran/appdecadastro/MainActivity.java
+++ app/src/main/java/com/unigran/appdecadastro/MainActivity.java
@@ -2,7 +2,10 @@ package com.unigran.appdecadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botaoProdutos = findViewById(R.id.botaoProdutos);
+        Button botaoClientes = findViewById(R.id.botaoClientes);
+        Button botaoFornecedores = findViewById(R.id.botaoFornecedores);
+        Button botaoSair = findViewById(R.id.botaoSair);
+
+        botaoProdutos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("segundaTela", 0);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoClientes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("segundaTela", 1);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoFornecedores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+                intent.putExtra("segundaTela", 2);
+
+                startActivity(intent);
+            }
+        });
+
+        botaoSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
     }
+    
 }
--- app/src/main/java/com/unigran/appdecadastro/SecondActivity.java
+++ app/src/main/java/com/unigran/appdecadastro/SecondActivity.java
@@ -1,8 +1,18 @@
 package com.unigran.appdecadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.unigran.appdecadastro.fragmentos.FragmentoCadastrarCliente;
+import com.unigran.appdecadastro.fragmentos.FragmentoCadastrarFornecedor;
+import com.unigran.appdecadastro.fragmentos.FragmentoCadastrarProduto;
+import com.unigran.appdecadastro.fragmentos.FragmentoListarCliente;
+import com.unigran.appdecadastro.fragmentos.FragmentoListarFornecedor;
+import com.unigran.appdecadastro.fragmentos.FragmentoListarProduto;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +20,70 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Integer segundaTelaOpcao = getIntent().getIntExtra("segundaTela", -1);
+
+        if (segundaTelaOpcao != -1) {
+            Button btnListagem = findViewById(R.id.botaoListagem);
+            Button btnCadastrar = findViewById(R.id.botaoCadastrar);
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            switch (segundaTelaOpcao) {
+                case 0: {
+                    transaction.add(R.id.fragmentoActivity, new FragmentoListarProduto());
+                    break;
+                }
+                case 1: {
+                    transaction.add(R.id.fragmentoActivity, new FragmentoListarCliente());
+                    break;
+                }
+                case 2: {
+                    transaction.add(R.id.fragmentoActivity, new FragmentoListarFornecedor());
+                    break;
+                }
+            }
+            transaction.commit();
+
+            btnListagem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switch (segundaTelaOpcao) {
+                        case 0: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentoActivity, new FragmentoListarProduto()).commit();
+                            break;
+                        }
+                        case 1: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentoActivity, new FragmentoListarCliente()).commit();
+                            break;
+                        }
+                        case 2: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentoActivity, new FragmentoListarFornecedor()).commit();
+                            break;
+                        }
+                    }
+                }
+            });
+
+            btnCadastrar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switch (segundaTelaOpcao) {
+                        case 0: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentoActivity, new FragmentoCadastrarProduto()).commit();
+                            break;
+                        }
+                        case 1: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentoActivity, new FragmentoCadastrarCliente()).commit();
+                            break;
+                        }
+                        case 2: {
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentoActivity, new FragmentoCadastrarFornecedor()).commit();
+                            break;
+                        }
+                    }
+                }
+            });
+        }
     }
 }
